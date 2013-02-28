0b0f1f96c28b2cb06731773e30cd196f63b1f80a
Chris Breiding <chrisbreiding@gmail.com>
2013-02-28 13:54:55 -0500

    ignore parts inside quotes in order to handle inline html

--- SublimeCoffeeClean.py
+++ SublimeCoffeeClean.py
@@ -1,4 +1,54 @@
 import sublime_plugin
+import string
+import re
+
+part_in_quotes = re.compile(r"([\"'])(?:\\\1|.)*?\1")
+
+replacements = [
+    # missing spaces
+    ['(?<=\S),(?=\S)', ', '],
+    [':(?=[(@=\'"-])', ': '],
+    ['=\{', '= {'],
+    ['=\[', '= ['],
+    ['=\(', '= ('],
+    ['=\(', '= ('],
+    ['=\'', '= \''],
+    ['="', '= "'],
+    ['(?<=[a-zA-Z0-9\'"])=', ' ='],
+
+    # extra spaces
+    ['[ \t]+=[ \t]+', ' = '],
+    ['(?<=\S)[ \t]+,', ','],
+    ['\[[ \t]+(?=\S)', '['],
+    ['(?<=\S)[ \t]+\]', ']'],
+    ['\([ \t]+(?=\S)', '('],
+    ['(?<=\S)[ \t]+\)', ')'],
+    ['\)\s+->', ')->'],
+    ['\)\s+=>', ')=>'],
+
+    # ensure a space before a lambda in parens
+    ['\((?=\([a-zA-Z0-9,]+\)-)', '( '],
+    ['\((?=\([a-zA-Z0-9,]+\)=)', '( '],
+
+    # quotes
+    ['""', "''"],
+
+    # misc
+    ["'use strict';", "'use strict'"],
+    ['"use strict";', "'use strict'"],
+    ['@$', 'this']
+]
+
+
+def is_between_quotes(region, view):
+    segment = view.substr(region)
+    line = view.substr(view.line(region))
+    match = re.search(part_in_quotes, line)
+
+    if not match:
+        return False
+
+    return string.find(match.group(0), segment) > -1
 
 
 class CoffeeCleanCommand(sublime_plugin.EventListener):
@@ -6,41 +56,6 @@ class CoffeeCleanCommand(sublime_plugin.EventListener):
     def on_pre_save(self, view):
         if len(view.file_name()) > 0 and view.file_name().endswith((".coffee")):
             edit = view.begin_edit()
-
-            replacements = [
-                # missing spaces
-                ['(?<=\S),(?=\S)', ', '],
-                [':(?=[(@=\'"-])', ': '],
-                ['=\{', '= {'],
-                ['=\[', '= ['],
-                ['=\(', '= ('],
-                ['=\(', '= ('],
-                ['=\'', '= \''],
-                ['="', '= "'],
-                ['(?<=[a-zA-Z0-9\'"])=', ' ='],
-
-                # extra spaces
-                ['[ \t]+=[ \t]+', ' = '],
-                ['(?<=\S)[ \t]+,', ','],
-                ['\[[ \t]+(?=\S)', '['],
-                ['(?<=\S)[ \t]+\]', ']'],
-                ['\([ \t]+(?=\S)', '('],
-                ['(?<=\S)[ \t]+\)', ')'],
-                ['\)\s+->', ')->'],
-                ['\)\s+=>', ')=>'],
-
-                # ensure a space before a lambda in parens
-                ['\((?=\([a-zA-Z0-9,]+\)-)', '( '],
-                ['\((?=\([a-zA-Z0-9,]+\)=)', '( '],
-
-                # quotes
-                ['""', "''"],
-
-                # misc
-                ["'use strict';", "'use strict'"],
-                ['"use strict";', "'use strict'"],
-                ['@$', 'this']
-            ]
 
             for replacement in replacements:
                 find = replacement[0]
@@ -50,6 +65,7 @@ class CoffeeCleanCommand(sublime_plugin.EventListener):
 
                 if regions:
                     for region in reversed(regions):
-                        view.replace(edit, region, replace)
+                        if not is_between_quotes(region, view):
+                            view.replace(edit, region, replace)
 
             view.end_edit(edit)
